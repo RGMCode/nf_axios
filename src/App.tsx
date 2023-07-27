f10ef24d1db77f01fc4311cde491e0b3f47a5635
mrg <rgmengedoth@gmail.com>
2023-07-27 16:05:57 +0200

    AXIOS - Get Post Put Delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,55 @@
 import './App.css'
 import axios from "axios";
 
+// ----------------Read----------------
  axios({
      method: 'get',
-     url: 'https://rickandmortyapi.com/api/character',
-     responseType: 'stream'
+     url: 'https://reqres.in/api/users?page=2',
  })
      .then(function (response) {
+         console.log("GET / READ")
          console.log("Response status ",response.status)
          console.log("Response data",response.data)
      });
+
+// ----------------Create----------------
+axios({
+    method: 'post',
+    url: 'https://reqres.in/api/users',
+    data: {
+        "name": "morpheus",
+        "job": "leader"
+    }
+}).then(function (response) {
+    console.log("POST / CREATE")
+    console.log("Response status ",response.status)
+    console.log("Response data",response.data)
+});
+
+// ----------------UPDATE----------------
+axios({
+    method: 'put',
+    url: 'https://reqres.in/api/users/2',
+    data: {
+        "name": "morpheus",
+        "job": "zion resident"
+    }
+}).then(function (response) {
+    console.log("PUT / UPDATE")
+    console.log("Response status ",response.status)
+    console.log("Response data",response.data)
+});
+
+// ----------------DELETE----------------
+axios({
+    method: 'delete',
+    url: 'https://reqres.in/api/users/2',
+}).then(function (response) {
+    console.log("DELETE")
+    console.log("Response status ",response.status)
+    console.log("Response data",response.data)
+});
+
 
  function App() {
      return null;
